3436e6cee83247ab200e82d3d2732f87ff2bbe35
Himanshu Yadav <ydv1608@gmail.com>
2020-08-06 01:38:12 +0530

    Added website code

---
+++ js/script_base.js
@@ -0,0 +1,37 @@
+var track = 1;
+
+function flip() {
+    track++;
+    if (track == 1)
+        document.body.style = "background-image:url(./../img/image1.jpg)";
+    if (track == 2)
+        document.body.style = "background-image:url(./../img/image2.jpg)";
+    if (track == 3)
+        document.body.style = "background-image:url(./../img/image3.jpg)";
+    if (track == 4)
+        document.body.style = "background-image:url(./../img/image4.jpg)";
+    if (track == 5) {
+        document.body.style = "background-image:url(./../img/image5.jpg)";
+        track = 0;
+    }
+}
+
+function gotohome() {
+    location.replace("./../index.html");
+}
+
+function gotohistory() {
+    location.replace("./html/project_air_force_history.html");
+}
+
+function gotomarshall() {
+    location.replace("./html/project_air_force_marshall.html");
+}
+
+function gotodefence() {
+    location.replace("./html/project_air_force_defend.html");
+}
+
+function gotovideo() {
+    location.replace("./html/project_air_force_video.html");
+}
